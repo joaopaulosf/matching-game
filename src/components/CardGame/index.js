d4cbd04b865a3bde03fb00bfb82b2990c1d5e54d
João P <100176817+joaopaulosf@users.noreply.github.com>
2023-04-18 17:54:20 -0300

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+            <img src="./src/images/vscode.png">
+        </article>
+    `;
+};
+
+export default CardGame;
