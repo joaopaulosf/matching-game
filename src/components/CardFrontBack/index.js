da42096fd60f880f23f5fe66bbf4394ad8755578
joaopaulosf <jpforcode@gmail.com>
2023-04-22 16:43:02 -0300

    :sparkles: Add card-flip to CardGame component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../CardGame";
+import "./style.css"
 
 const CardFrontBack = () => { 
     return `
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("vue-js", "vue-icon")}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame("vue-js", "vue-icon")}
+            </div>
         </article>
     `
 };
