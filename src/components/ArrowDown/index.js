ebcfd29a73aa0d2a6b5eee5767e0e4ad92597f26
joaopaulosf <jpforcode@gmail.com>
2023-04-24 17:48:42 -0300

    :construction: Create ArrownDown component

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,7 @@
+const ArrowDown = () => {
+    return `
+        <img src="./src/images/arrow-down.svg" alt="arrow-down">
+    `
+};
+
+export default ArrowDown;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import "./style.css"
 import PlayerName from "../../components/PlayerName"
 import PlayerVersus from "../../components/PlayerVersus"
 import PlayerScore from "../../components/PlayerScore"
+import ArrowDown from "../../components/ArrowDown"
 
 const ScoreBoard = () => {
     return `
         <header class="score-board">
+            ${ArrowDown()}
             <section class="player-one">
                 ${PlayerName("Player 1")}
                 ${PlayerScore(2)}
@@ -13,7 +15,7 @@ const ScoreBoard = () => {
             ${PlayerVersus()}
             <section class="player-two">
                 ${PlayerName("Player 2")}
-                ${PlayerScore(1)}
+                ${PlayerScore(2)}
             </section>
         </header>
     `
