9714b423c238c100cf55e3106e705688a9e680c3
joaopaulosf <jpforcode@gmail.com>
2023-04-24 20:22:49 -0300

    :sparkles: add hideCards and playerSwap to GameBoard

--- src/objects/GameBoard/index.js
+++ src/objects/GameBoard/index.js
@@ -4,11 +4,34 @@ import cards from "./data";
 
 
 const BoardGame = () => {
+    const hideCards = ($cardsActive) => {
+        $cardsActive.forEach((card) => card.classList.remove("-active"))
+    }
+
+    const playerSwap = () => {
+        const $arrowDown = document.querySelector(".arrow-down")
+        const currentPlayer = $arrowDown.getAttribute("data-currentplayer")
+
+        $arrowDown.setAttribute("data-currentplayer", currentPlayer == 1 ? 2 : 1)
+    }
+
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        const $boardGame = document.querySelector(".board-game");
+        const $cardsActive = $boardGame.querySelectorAll(".card-front-back.-active")
+
+        if($cardsActive.length === 2) {
+            setTimeout(() => {
+                playerSwap()
+                hideCards($cardsActive)
+            }, 700);
+        }
+    }
     const htmlCardsList = cards.map((card)=> CardFrontBack(card.icon, card.altIcon));
     const $htmlCards = htmlCardsList.join("")
 
     return `
-        <section class="board-game">
+        <section class="board-game" onclick="boardGame.handleClick()">
             ${$htmlCards}
         </section>
     `;
